795928506fa56b138d22171c7b32e7cc192816f0
tedhoon <gt0305@likelion.org>
2020-08-10 16:40:12 +0900

    [add] Dog Breed data migration

--- django-testcase/testbackend/datas/admin.py
+++ django-testcase/testbackend/datas/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Disease, NutrientData
+from .models import Disease, NutrientData, DogBreed
 from import_export.admin import ImportExportMixin
 
 class DiseaseAdmin(ImportExportMixin, admin.ModelAdmin):
@@ -14,9 +14,18 @@ admin.site.register(Disease, DiseaseAdmin)
 
 class NutrientDataAdmin(ImportExportMixin, admin.ModelAdmin):
     list_display = ('name', 'desc', )
-    list_editable =  ('desc', )
-    fields = ('name', 'desc', 'disease')
+    # list_editable =  ('desc', )
+    # fields = ('name', 'desc', )
     search_fields = ('name', 'desc', 'disease')
     pass
 
 admin.site.register(NutrientData, NutrientDataAdmin)
+
+class DogBreedAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ('id', 'name', )
+    list_editable =  ('name',)
+    # fields = ('name', )
+    search_fields = ('name', )
+    pass
+
+admin.site.register(DogBreed, DogBreedAdmin)
--- django-testcase/testbackend/datas/models.py
+++ django-testcase/testbackend/datas/models.py
@@ -8,11 +8,13 @@ class AgeRange(models.Model):
 
     name = models.CharField('연령', max_length=20)
     desc = models.TextField('설명', blank=True)
-    min_age = models
-    max_age = 
+    # min_age = models.
+    # max_age = models.
+    # 이처럼 age range를 받고,
+    # wp에는... 해당 나이에 대한 질병 데이터가 있숨.... => 그래서 그냥 그걸 데이터로 띄워주는데 이것 필요할까?
+    # 
     def __str__(self):
         return self.name
-
 
 
 
@@ -39,5 +41,16 @@ class NutrientData(models.Model):
     desc = models.TextField('설명', blank=True)
     disease = models.ManyToManyField(Disease, verbose_name="관련 질병", blank=True, null=True) 
     
+    def __str__(self):
+        return self.name
+
+
+class DogBreed(models.Model):
+    class Meta:
+        verbose_name = "품종 데이터"
+        verbose_name_plural = "품종 데이터"
+
+    name = models.CharField('견종', max_length=20)
+
     def __str__(self):
         return self.name
