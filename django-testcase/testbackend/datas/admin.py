3e3cd477dc8855a4ef4e976a31b5c69b0075bb76
tedhoon <gt0305@likelion.org>
2020-08-11 16:27:19 +0900

    [add] agerange model

--- django-testcase/testbackend/datas/admin.py
+++ django-testcase/testbackend/datas/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Disease, NutrientData, DogBreed
+from .models import Disease, NutrientData, DogBreed, AgeRange
 from import_export.admin import ImportExportMixin
 
 class DiseaseAdmin(ImportExportMixin, admin.ModelAdmin):
@@ -29,3 +29,14 @@ class DogBreedAdmin(ImportExportMixin, admin.ModelAdmin):
     pass
 
 admin.site.register(DogBreed, DogBreedAdmin)
+
+class AgeRangeAdmin(ImportExportMixin, admin.ModelAdmin):
+    list_display = ('name', 'desc')
+    list_editable =  ('desc',)
+    fields = ('name', 'slug', 'desc', 'min_age', 'max_age', 'disease', ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'l', 'n', 'o', 'p'))
+    search_fields = ('name', )
+    pass
+
+admin.site.register(AgeRange, AgeRangeAdmin)
+
+# ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm', 'l')
--- django-testcase/testbackend/datas/models.py
+++ django-testcase/testbackend/datas/models.py
@@ -1,24 +1,5 @@
 from django.db import models
 
-class AgeRange(models.Model):
-      
-    class Meta:
-        verbose_name = "연령 데이터"
-        verbose_name_plural = "연령 데이터"
-
-    name = models.CharField('연령', max_length=20)
-    desc = models.TextField('설명', blank=True)
-    # min_age = models.
-    # max_age = models.
-    # 이처럼 age range를 받고,
-    # wp에는... 해당 나이에 대한 질병 데이터가 있숨.... => 그래서 그냥 그걸 데이터로 띄워주는데 이것 필요할까?
-    # 
-    def __str__(self):
-        return self.name
-
-
-
-        
 class Disease(models.Model):
       
     class Meta:
@@ -27,7 +8,7 @@ class Disease(models.Model):
 
     name = models.CharField('질병 이름', max_length=20)
     desc = models.TextField('설명', blank=True)
-    
+
     def __str__(self):
         return self.name
 
@@ -43,6 +24,44 @@ class NutrientData(models.Model):
     
     def __str__(self):
         return self.name
+
+
+class AgeRange(models.Model):
+      
+    class Meta:
+        verbose_name = "연령 데이터"
+        verbose_name_plural = "연령 데이터"
+    # temp = models.CharField
+    name = models.CharField('연령', max_length=20)
+    slug = models.SlugField('슬러그', blank=True, null=True)
+    desc = models.TextField('설명', blank=True)
+    min_age = models.PositiveIntegerField("최소 개월수", blank=True, null=True)
+    max_age = models.PositiveIntegerField("최대 개월수", blank=True, null=True)
+    # 이처럼 age range를 받고,
+    # caution_disease = models.TextField('주의 질병(피부/관절/눈/심장/신장/방광/치아/간/췌장염/종양/비타민결핍/칼슘결핍/면역력결핍)', null=True, blank=True)
+    disease = models.ManyToManyField(Disease, verbose_name="관련 질병", blank=True, null=True) 
+    # wp에는... 해당 나이에 대한 질병 데이터가 있숨.... => 그래서 그냥 그걸 데이터로 띄워주는데 이것 필요할까?
+        
+    a = models.PositiveIntegerField('피부(위험도 %)', null=True, blank=True)
+    b = models.PositiveIntegerField('관절(위험도 %)', null=True, blank=True)
+    c = models.PositiveIntegerField('눈(위험도 %)', null=True, blank=True)
+    d = models.PositiveIntegerField('심장(위험도 %)', null=True, blank=True)
+    e = models.PositiveIntegerField('신장(위험도 %)', null=True, blank=True)
+    f = models.PositiveIntegerField('방광(위험도 %)', null=True, blank=True)
+    g = models.PositiveIntegerField('치아(위험도 %)', null=True, blank=True)
+    h = models.PositiveIntegerField('간(위험도 %)', null=True, blank=True)
+    i = models.PositiveIntegerField('췌장염(위험도 %)', null=True, blank=True)
+    j = models.PositiveIntegerField('종양(위험도 %)', null=True, blank=True)
+    k = models.PositiveIntegerField('비타민결핍(위험도 %)', null=True, blank=True)
+    m = models.PositiveIntegerField('칼슘결핍(위험도 %)', null=True, blank=True)
+    l = models.PositiveIntegerField('면역력결핍(위험도 %)', null=True, blank=True)
+    n = models.PositiveIntegerField('장건강(위험도 %)', null=True, blank=True)
+    o = models.PositiveIntegerField('비만(위험도 %)', null=True, blank=True)
+    p = models.PositiveIntegerField('호흡기(위험도 %)', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
 
 
 class DogBreed(models.Model):
